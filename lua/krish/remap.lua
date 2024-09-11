9ce982a0b96bb6925cf2d97c338ba400e1ae6cac
Yadhukrishnan P <yadhukrishnannair99@gmail.com>
2024-09-10 22:19:20 -0700

    changes

--- lua/krish/remap.lua
+++ lua/krish/remap.lua
@@ -1,10 +1,11 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><Esc>", vim.cmd.Ex)
 
+-- move selected text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- move from different windows
 vim.keymap.set("n", "<leader>k", "<C-w>k")
 vim.keymap.set("n", "<leader>h", "<C-w>h")
 vim.keymap.set("n", "<leader>j", "<C-w>j")
